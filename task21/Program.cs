5c8dfcc1a526d476d4e35a9e65196e5567bfd4d2
Roman <vvptus@yandex.ru>
2022-11-01 20:41:12 +0300

    Lesson 7-two dimensional array

---
+++ task21/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 1. Задайте двумерный массив размером m×n, заполненный случайными вещественными 
+// числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+double[,] matr = new double[3, 4];
+
+void Fillcoll(double[,] mat,int minValue, int maxValue)
+{
+    for (int i = 0; i < mat.GetLength(0); i++)
+    {
+        for (int j = 0; j < mat.GetLength(1); j++)
+        {
+            Random rnd = new Random();
+            mat[i, j] = rnd.Next(minValue, maxValue)+rnd.NextDouble();
+        }
+    }
+}
+
+void PrintArray(double[,] col)
+{
+    for (int i = 0; i < col.GetLength(0); i++)
+    {
+        for (int j = 0; j < col.GetLength(1); j++)
+        {
+            System.Console.Write($"{col[i, j]:f1}   ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+Fillcoll(matr,-10,10);
+
+PrintArray(matr);
---
+++ task22/Program.cs
@@ -0,0 +1,65 @@
+﻿// Задача 2. Напишите программу, которая на вход принимает позиции элемента в
+// двумерном массиве, и возвращает значение этого элемента или же указание,
+//  что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 1, 7 -> такого числа в массиве нет
+
+
+int[,] matr = new int[3, 4];
+
+
+void Fillcoll(int[,] mat, int minValue, int maxValue)
+{
+    for (int i = 0; i < mat.GetLength(0); i++)
+    {
+        for (int j = 0; j < mat.GetLength(1); j++)
+        {
+            Random rnd = new Random();
+            mat[i, j] = rnd.Next(minValue, maxValue);
+        }
+    }
+}
+
+void PrintArray(int[,] col)
+    {
+        for (int i = 0; i < col.GetLength(0); i++)
+        {
+            for (int j = 0; j < col.GetLength(1); j++)
+            {
+                Console.Write($"{col[i, j]}   ");
+            }
+            Console.WriteLine();
+        }
+    }
+    
+int Prompt(string message)
+{
+ Console.WriteLine(message);
+int value = Convert.ToInt32(Console.ReadLine());
+return value;
+}
+void ValidatePositionMatrix(int[,] coll, int arg, int arg1)
+{
+int value = 0;
+int value1 = 0;
+for (int i = 0; i < coll.GetLength(0); i++)
+{
+if (i == arg)
+{
+for (int j = 0; j < coll.GetLength(1); j++)
+{
+if (j == arg1)
+{
+value1 = arg1;
+}
+else
+{
+
+Console.WriteLine($"{value}, {value1}");
+}
+Console.WriteLine("Такого числа в массиве нет");
+}
+
